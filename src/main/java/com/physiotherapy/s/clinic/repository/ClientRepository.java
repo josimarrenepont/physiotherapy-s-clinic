b90313e2f79384d2f035986d71210ac8824591a7
josimarrenepont <jnrenepont@hotmail.com>
2024-03-06 14:27:18 -0300

    Created ClientRepository Class

---
+++ src/main/java/com/physiotherapy/s/clinic/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.physiotherapy.s.clinic.repository;
+
+import com.physiotherapy.s.clinic.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
