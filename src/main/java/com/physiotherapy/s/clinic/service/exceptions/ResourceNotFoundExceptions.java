f7add3da2ad3e2d74f6e358096a0c159eb2de980
josimarrenepont <jnrenepont@hotmail.com>
2024-03-15 22:50:54 -0300

    Created ResourceNotFoundExceptions

---
+++ src/main/java/com/physiotherapy/s/clinic/service/exceptions/ResourceNotFoundExceptions.java
@@ -0,0 +1,8 @@
+package com.physiotherapy.s.clinic.service.exceptions;
+
+public class ResourceNotFoundExceptions extends RuntimeException{
+
+    public ResourceNotFoundExceptions(Object id){
+        super("Resource Not Found " + id);
+    }
+}
