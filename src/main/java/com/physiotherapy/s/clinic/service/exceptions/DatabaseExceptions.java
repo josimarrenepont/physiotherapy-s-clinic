16ab9f092025b963370613a8f78f49a172ee8283
josimarrenepont <jnrenepont@hotmail.com>
2024-03-15 22:48:48 -0300

    Created DatabaseExceptions

---
+++ src/main/java/com/physiotherapy/s/clinic/service/exceptions/DatabaseExceptions.java
@@ -0,0 +1,8 @@
+package com.physiotherapy.s.clinic.service.exceptions;
+
+public class DatabaseExceptions extends RuntimeException{
+
+    public DatabaseExceptions(String msg){
+        super(msg);
+    }
+}
