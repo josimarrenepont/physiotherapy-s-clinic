bb6c4cacdccd120733251c721bd85ba53f7a1065
josimarrenepont <jnrenepont@hotmail.com>
2024-04-18 22:53:17 -0300

    Created AccountCredentialsVO and InvalidJwtAuthenticationException

--- src/main/java/com/physiotherapy/s/clinic/controller/exceptions/ControllerExceptionsHandler.java
+++ src/main/java/com/physiotherapy/s/clinic/controller/exceptions/ControllerExceptionsHandler.java
@@ -1,12 +1,14 @@
 package com.physiotherapy.s.clinic.controller.exceptions;
 
 import com.physiotherapy.s.clinic.service.exceptions.DatabaseExceptions;
+import com.physiotherapy.s.clinic.service.exceptions.InvalidJwtAuthenticationException;
 import com.physiotherapy.s.clinic.service.exceptions.ResourceNotFoundExceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
 
@@ -25,6 +27,13 @@ public class ControllerExceptionsHandler {
     public ResponseEntity<StandardError> database(DatabaseExceptions e, HttpServletRequest request){
         String error = "Database error";
         HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public ResponseEntity<StandardError> handleInvalidJwtAuthenticationException(InvalidJwtAuthenticationException e, HttpServletRequest request){
+        String error = "Invalid Authentication";
+        HttpStatus status = HttpStatus.FORBIDDEN;
         StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
     }
